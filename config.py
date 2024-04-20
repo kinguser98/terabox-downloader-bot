6b07bd8374628d10acba292d1e288f76bfb27728
kinguser98 <121923367+kinguser98@users.noreply.github.com>
2024-04-20 16:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BOT_TOKEN = "6683891175:AAG7Ub8tNlukccbvpe_MK1DBuVOIsKYdq14"  # bot token
 
 
 ## REDIS
-HOST = "redis-12320.c299.asia-northeast1-1.gce.cloud.redislabs.com"  # redis host uri
-PORT = -1001999304148  # redis port
-PASSWORD = "0NJmIimtiBDX83W20BS2bypFuHYKrfnN"  # redis password
+HOST = "redis-12722.c74.us-east-1-4.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 12722  # redis port
+PASSWORD = "wBnSTIOiMCw2N8lsDcfMq0ScDkxAHKV4"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1001999304148  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = "csrfToken=Hxf_lu5FJL4J0r67dWnaGrhc; lang=en; TSID=eeRnKP1it7v9AB9b6xqJEiIwyL8mYcxY; __bid_n=18edb1deff14b4744f4207; _ga=GA1.1.2112040923.1713073157; __stripe_mid=28add43a-9e57-44bb-b28b-8cbaae076d3b38a5b4; ndus=YT24CKxteHuiWsW-vu5E_AoIBCtB4PGWqfcJ8G4r; browserid=6pHvmG9AgZFsosafSp0kwJi8SQhuJ604SSwki68GfqzdvDgJ5wc_5f-08gg=; _ga_06ZNKL8C2E=GS1.1.1713424948.5.1.1713425135.60.0.0; ndut_fmt=46158B22AFA762FF7904119B15593DCBFBE0A4D24BA623FD19960E574DE3F475;"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [766716953]
