8f8c0b5d86b3bb68c255be7955243cbf4139599e
kinguser98 <121923367+kinguser98@users.noreply.github.com>
2024-04-20 16:00:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,13 +47,13 @@ Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
 Join @RoldexVerse For Updates
 [Source Code](https://github.com/r0ld3x/terabox-downloader-bot) """
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @updateschaneeee then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @updateschaneeee then send me the link again."
         )
     await m.reply(reply_text, link_preview=False, parse_mode="markdown")
 
@@ -69,13 +69,13 @@ Join @RoldexVerse For Updates
 async def start(m: UpdateNewMessage):
     text = m.pattern_match.group(1)
     fileid = db.get(str(text))
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @updateschaneeee then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @updateschaneeee then send me the link again."
         )
     await bot(
         ForwardMessagesRequest(
@@ -126,16 +126,16 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @updateschaneeee then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@updateschaneeee", m.peer_id)
     if not check_if:
         return await m.reply(
             "Please join @RoldexVerseChats then send me the link again."
         )
     is_spam = db.get(m.sender_id)
-    if is_spam and m.sender_id not in [1317173146]:
+    if is_spam and m.sender_id not in [766716953]:
         return await m.reply("You are spamming. Please wait a 1 minute and try again.")
     hm = await m.reply("Sending you the media wait...")
     count = db.get(f"check_{m.sender_id}")
@@ -187,7 +187,7 @@ async def handle_message(m: Message):
         return await hm.edit(
             f"Sorry! File is not supported for now. I can download only .mp4, .mkv and .webm files."
         )
-    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [1317173146]:
+    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [766716953]:
         return await hm.edit(
             f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} ."
         )
@@ -239,7 +239,7 @@ File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
 Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
 
-@RoldexVerse
+@updateschaneeee
 """,
             supports_streaming=True,
             spoiler=True,
@@ -263,7 +263,7 @@ File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
 Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
 
-@RoldexVerse
+@updateschaneeee
 """,
             progress_callback=progress_bar,
             thumb=thumbnail if thumbnail else None,
