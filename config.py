86c8f152a0313828f5227739fd500dc0b6c86343
kinguser98 <121923367+kinguser98@users.noreply.github.com>
2024-04-20 15:49:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 23391941  # api id
+API_HASH = "392f541aae8545711de9d77e8d324b82"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "6683891175:AAG7Ub8tNlukccbvpe_MK1DBuVOIsKYdq14"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-12320.c299.asia-northeast1-1.gce.cloud.redislabs.com"  # redis host uri
+PORT = -1001999304148  # redis port
+PASSWORD = "0NJmIimtiBDX83W20BS2bypFuHYKrfnN"  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+COOKIE = "csrfToken=Hxf_lu5FJL4J0r67dWnaGrhc; lang=en; TSID=eeRnKP1it7v9AB9b6xqJEiIwyL8mYcxY; __bid_n=18edb1deff14b4744f4207; _ga=GA1.1.2112040923.1713073157; __stripe_mid=28add43a-9e57-44bb-b28b-8cbaae076d3b38a5b4; ndus=YT24CKxteHuiWsW-vu5E_AoIBCtB4PGWqfcJ8G4r; browserid=6pHvmG9AgZFsosafSp0kwJi8SQhuJ604SSwki68GfqzdvDgJ5wc_5f-08gg=; _ga_06ZNKL8C2E=GS1.1.1713424948.5.1.1713425135.60.0.0; ndut_fmt=46158B22AFA762FF7904119B15593DCBFBE0A4D24BA623FD19960E574DE3F475;"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [766716953]
